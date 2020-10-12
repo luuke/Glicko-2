f5f425ea088a4e79ba7ad280c5693b885592d364
Luuke <lukasz.bartosz1990@gmail.com>
2020-10-12 07:54:17 +0200

    Created Rating class

---
+++ Rating.h
@@ -0,0 +1,16 @@
+#ifndef __Rating_H
+#define __Rating_H
+
+class Rating
+{
+public:
+    Rating();
+
+private:
+    float rating;
+    float deviation;
+    float volatility;
+};
+
+#endif // __Rating_H
+
