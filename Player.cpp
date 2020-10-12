eb38b2a743174a2ecc478070247d95c585f776a7
Luuke <lukasz.bartosz1990@gmail.com>
2020-10-12 13:23:36 +0200

    Added constructor, id, name and rating

--- Player.cpp
+++ Player.cpp
@@ -1,2 +1,14 @@
+#include <sstream>
 #include "Player.h"
 
+Player::Player(uint32_t id)
+    : m_ID(id)
+{
+}
+
+std::string Player::GetName(void)
+{
+    std::stringstream ss;
+    ss << "Player_" << m_ID;
+    return ss.str();
+}
--- Player.h
+++ Player.h
@@ -1,8 +1,20 @@
 #ifndef __Player_H
 #define __Player_H
 
+#include <stdint.h>
+#include <string>
+#include "Rating.h"
+
 class Player
 {
+public:
+    Player(uint32_t id);
+
+    Rating m_Rating;
+
+    std::string GetName(void);
+private:
+    uint32_t m_ID;
 };
 
 #endif // __Player_H
