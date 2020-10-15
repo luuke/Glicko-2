cbd7c411f696439cbc74c6178b1194422f28635d
Luuke <lukasz.bartosz1990@gmail.com>
2020-10-15 13:57:00 +0200

    Added converting to Glicko-2 scale

--- Glicko2.cpp
+++ Glicko2.cpp
@@ -1,2 +1,21 @@
 #include "Glicko2.h"
 
+void Glicko2::CalculateNewRating(GlickoRating playerRating, GlickoRating opponentRating, float playerScore)
+{
+    // Step 1/2: Determine player rating and convert to Glicko-2 scale 
+    Glicko2Rating player{ u(playerRating.m_Rating), phi(playerRating.m_Deviation), playerRating.m_Volatility };
+    Glicko2Rating opponent{ u(opponentRating.m_Rating), phi(opponentRating.m_Deviation), opponentRating.m_Volatility };
+
+}
+
+float Glicko2::u(float r)
+{
+    return (r - 1500.0f) / 173.7178f;
+}
+
+float Glicko2::phi(float RD)
+{
+    return RD / 173.7178f;
+}
+
+
--- Glicko2.h
+++ Glicko2.h
@@ -1,8 +1,25 @@
 #ifndef __Glicko2_H
 #define __Glicko2_H
 
+#include "GlickoRating.h"
+
 class Glicko2
 {
+    struct Glicko2Rating
+    {
+        float u;
+        float phi;
+        float sigma;
+    };
+
+public:
+    void CalculateNewRating(GlickoRating playerRating, GlickoRating opponentRating, float playerScore);
+
+private:
+    float u(float r);
+    float phi(float RD);
+
+
 };
 
 #endif // __Glicko2_H
