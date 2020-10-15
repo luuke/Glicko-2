7f59fddd10cfad468bfdc97f9fe220cba34e3f43
Luuke <lukasz.bartosz1990@gmail.com>
2020-10-15 15:02:58 +0200

    Calculating quantity v

--- Glicko2.cpp
+++ Glicko2.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include "Glicko2.h"
 
 void Glicko2::CalculateNewRating(GlickoRating playerRating, GlickoRating opponentRating, float playerScore)
@@ -6,6 +7,8 @@ void Glicko2::CalculateNewRating(GlickoRating playerRating, GlickoRating opponen
     Glicko2Rating player{ u(playerRating.m_Rating), phi(playerRating.m_Deviation), playerRating.m_Volatility };
     Glicko2Rating opponent{ u(opponentRating.m_Rating), phi(opponentRating.m_Deviation), opponentRating.m_Volatility };
 
+    // Step 3: Compute quantity v
+    float tmp_v = v(player.u, opponent.u, opponent.phi);
 }
 
 float Glicko2::u(float r)
@@ -18,4 +21,20 @@ float Glicko2::phi(float RD)
     return RD / 173.7178f;
 }
 
+float Glicko2::g(float phi)
+{
+    return (1 / sqrt(1 + ((3 * powf(phi, 2)) / (powf(pi(), 2)))));
+}
 
+float Glicko2::E(float u, float u_j, float phi_j)
+{
+    return (1 / (1 + expf(-1 * g(phi_j) * (u - u_j))));
+}
+
+float Glicko2::v(float u, float u_j, float phi_j)
+{
+    float tmp_g = g(phi_j);
+    float tmp_E = E(u, u_j, phi_j);
+
+    return powf((powf(tmp_g, 2)* tmp_E* (1 - tmp_E)), -1);
+}
--- Glicko2.h
+++ Glicko2.h
@@ -16,10 +16,13 @@ public:
     void CalculateNewRating(GlickoRating playerRating, GlickoRating opponentRating, float playerScore);
 
 private:
+    constexpr float pi() { return (2 * std::asin(1.0)); }
+
     float u(float r);
     float phi(float RD);
-
-
+    float g(float phi);
+    float E(float u, float u_j, float phi_j);
+    float v(float u, float u_j, float phi_j);
 };
 
 #endif // __Glicko2_H
