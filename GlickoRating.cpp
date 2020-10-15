c18f27c83906bc7b9f039d7acf79d8ca5b2d2cdf
Luuke <lukasz.bartosz1990@gmail.com>
2020-10-15 09:50:37 +0200

    Removed scaling to Glicko-2 system

--- GlickoRating.cpp
+++ GlickoRating.cpp
@@ -1,28 +1,7 @@
 #include "GlickoRating.h"
 
 GlickoRating::GlickoRating(float rating, float deviation, float volatility)
+    : m_Rating(rating), m_Deviation(deviation), m_Volatility(volatility)
 {
-    SetRating(rating);
-    SetDeviation(deviation);
-    m_Volatility = volatility;
 }
 
-void GlickoRating::SetRating(float r)
-{
-    m_Rating = (r - 1500.0f) / 173.7178f;
-}
-
-float GlickoRating::GetRating(void)
-{
-    return m_Rating * 173.7178f + 1500.0f;
-}
-
-void GlickoRating::SetDeviation(float rd)
-{
-    m_Deviation = rd / 173.7178f;
-}
-
-float GlickoRating::GetDeviation(void)
-{
-    return m_Deviation * 173.7178f;
-}
--- GlickoRating.h
+++ GlickoRating.h
@@ -6,13 +6,6 @@ class GlickoRating
 public:
     GlickoRating(float rating = 1500.0f, float deviation = 350.0f, float volatility = 0.06f);
 
-    void SetRating(float r);
-    float GetRating(void);
-
-    void SetDeviation(float rd);
-    float GetDeviation(void);
-
-private:
     float m_Rating;
     float m_Deviation;
     float m_Volatility;
